70ad219df44326c2eda87bb6f5ef6344bfd3aec9
mfbrockway-hub <mfbrockway@gmail.com>
2026-04-14 17:29:15 -0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Business Checkbook',
+  description: 'Simple running ledger app',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
